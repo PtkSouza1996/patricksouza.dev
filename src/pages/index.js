d428f46d58ebda7ca3f89e8238887f2f1486eed9
Patrick souza <patrick.souza96@outlook.com>
2020-04-04 18:57:34 -0300

    refactor home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Header, TopSkills, Skills, WorkExperience } from '../modules/Cv';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
-
-export default IndexPage;
+export default function CvPage() {
+  return (
+    <Layout>
+      <SEO title="Currículo" />
+      <Header />
+      <TopSkills />
+      <Skills />
+      <WorkExperience />
+    </Layout>
+  );
+}
